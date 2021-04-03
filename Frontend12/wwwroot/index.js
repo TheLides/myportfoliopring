c4a5e6eff45f5f0fe4776161987ca0d4c803ffe0
Rina Trofimova <lidesrose@gmail.com>
2021-04-04 02:02:13 +0700

    Third part done

--- Frontend12/wwwroot/index.js
+++ Frontend12/wwwroot/index.js
@@ -1,15 +1,9 @@
 ﻿document.addEventListener('DOMContentLoaded', () => {
-    let OwnerName = document.getElementById('OwnerName');
-    let KittyName = document.getElementById('CatName');
-    let Password = document.getElementById('Password');
-    let ConfirmPassword = document.getElementById('ConfirmPassword');
-    let Breed = document.getElementById('Breed');
-    document.getElementById('SignBtn').addEventListener('click', () => {
-        document.getElementById('OwnerNameModal').value = OwnerName.value;
-        document.getElementById('CatNameModal').value = KittyName.value;
-        document.getElementById('PasswordModal').value = Password.value;
-        document.getElementById('ConfirmPasswordModal').value = ConfirmPassword.value;
-        document.getElementById('BreedModal').value = Breed.value;
-        document.getElementById('exampleModalLabel').innerText = `New account for ${KittyName.value}`;
-    });
+
+    for (let i = 0; i < 3; i++) {
+        let card = document.querySelector('.myCardCol').cloneNode(true);
+        let parent = document.querySelector('.myCardCol').parentNode;
+        parent.appendChild(card);
+    }
+
 });
